4984ca2eef8a468dccf3b5c0234915c319371787
Ahmed Sami <ahmed222333sami@gmail.com>
2026-01-01 15:35:08 +0200

    init

---
+++ src/main/java/com/example/netflix/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.example.netflix;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello, World!";
+    }
+    
+}
