4443dca094b5e775d510253f0cb6cd5bc349d226
Ahmed Sami <ahmed222333sami@gmail.com>
2026-01-05 06:39:14 +0200

    docker init config

--- src/main/java/com/example/netflix/HelloWorldController.java
+++ src/main/java/com/example/netflix/HelloWorldController.java
@@ -3,13 +3,13 @@ package com.example.netflix;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @RestController
 public class HelloWorldController {
 
     @GetMapping("/hello")
     public String helloWorld() {
+       
         return "Hello, World!";
     }
-    
+
 }
